f79d24a6159fefac7e0756792c3275e1d09c30ed
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-03-04 23:19:51 +0100

    fixed hello world

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
     </>
